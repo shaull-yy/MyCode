d9523be07b6c9b52322f027f6c1e028acbd38bae
shaul_yy <shaull@yahoo.com>
2025-05-31 16:43:44 +0300

    new function for file clean up

--- code/My_Utilities/utl_functions_01.py
+++ code/My_Utilities/utl_functions_01.py
@@ -19,6 +19,50 @@ def are_lists_same(list1, list2, check_vals_and_order): #Return values: False - 
 	return True
 
 
+import os
+import glob
+import time
+from datetime import datetime, timedelta
+
+def clean_files_keep_x_recent(folder_path, file_name_pattern, keep_x_files, only_b4_x_days):
+	# Arguments:
+	# file_name_pattern should be like 'trans_file_*.xlsx'
+	# keep_x_files - we delete all files and keeping "keep_x_files" most recent files
+	# only_b4_x_days - In addition to above condition, we only delete files that were modified "only_b4_x_days" days ago
+
+	del_count = 0
+	if folder_path == '':
+		file_pattern = file_name_pattern
+	else:
+	# Get all files matching the pattern
+		file_pattern = os.path.join(folder_path, file_name_pattern)
+	
+	# Get the current time and calculate the cutoff time (12 days ago) 
+	cutoff_time = time.time() - (only_b4_x_days * 24 * 60 * 60) # 12 * 24 * 60 * 60 is 12 deys in minutes
+
+	files = glob.glob(file_pattern)
+	
+	# Filter files based on modification time older than 12 days
+	files = [file for file in files if os.path.getmtime(file) < cutoff_time]
+	# Sort files by modification time (newest first)
+	files_sorted = sorted(files, key=os.path.getmtime, reverse=True)
+
+	# Keep the last keep_x_files files
+	files_to_delete = files_sorted[keep_x_files:]
+
+	# Delete the remaining files
+	for file in files_to_delete:
+		try:
+			os.remove(file)
+			del_count += 1
+			print(f"File : {file} was deleted")
+		except Exception as e:
+			print(f"Error deleting {file}: {e}")
+
+	print(f"Cleanup completed.  {del_count} files were deleted")
+	return del_count
+
+
 if __name__ == '__main__':
 	list1 = [1,2,3,4,5]
 	list2 = [1,2,3,4,5]
@@ -28,6 +72,3 @@ if __name__ == '__main__':
 	print('--------------')
 	print(are_lists_same(list1,list3,True))
 	print(are_lists_same(list1,list3,False))
-	
-
-
